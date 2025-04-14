7aa8b7b8ce72447f5e0b7c8295a6528c111e11f5
jhfnetboy <jhfnetboy@gmail.com>
2025-04-14 11:51:05 +0700

    flat submodule

---
+++ src/flat_SuperPaymaster.sol
@@ -0,0 +1,319 @@
+
+/** 
+ *  SourceUnit: /Users/jason/Dev/Community/SuperPaymaster-Contract/src/SuperPaymaster.sol
+*/
+            
+////// SPDX-License-Identifier-FLATTEN-SUPPRESS-WARNING: MIT
+// OpenZeppelin Contracts (last updated v5.0.1) (utils/Context.sol)
+
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+
+    function _contextSuffixLength() internal view virtual returns (uint256) {
+        return 0;
+    }
+}
+
+
+
+
+/** 
+ *  SourceUnit: /Users/jason/Dev/Community/SuperPaymaster-Contract/src/SuperPaymaster.sol
+*/
+            
+////// SPDX-License-Identifier-FLATTEN-SUPPRESS-WARNING: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * ////IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 value) external returns (bool);
+
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+
+
+
+/** 
+ *  SourceUnit: /Users/jason/Dev/Community/SuperPaymaster-Contract/src/SuperPaymaster.sol
+*/
+            
+////// SPDX-License-Identifier-FLATTEN-SUPPRESS-WARNING: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.20;
+
+////import {Context} from "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * The initial owner is set to the address provided by the deployer. This can
+ * later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    /**
+     * @dev The caller account is not authorized to perform an operation.
+     */
+    error OwnableUnauthorizedAccount(address account);
+
+    /**
+     * @dev The owner is not a valid owner account. (eg. `address(0)`)
+     */
+    error OwnableInvalidOwner(address owner);
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
+     */
+    constructor(address initialOwner) {
+        if (initialOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(initialOwner);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        if (owner() != _msgSender()) {
+            revert OwnableUnauthorizedAccount(_msgSender());
+        }
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        if (newOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+
+/** 
+ *  SourceUnit: /Users/jason/Dev/Community/SuperPaymaster-Contract/src/SuperPaymaster.sol
+*/
+
+////// SPDX-License-Identifier-FLATTEN-SUPPRESS-WARNING: MIT
+pragma solidity 0.8.23;
+
+////import "@openzeppelin/contracts/access/Ownable.sol";
+////import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+////import "@ensdomains/ens-contracts/contracts/registry/ENS.sol";
+////import "@ensdomains/ens-contracts/contracts/resolvers/Resolver.sol";
+////import "./interfaces/IPaymaster.sol";
+
+contract SuperPaymaster is Ownable {
+    struct PaymasterInfo {
+        address paymasterAddress;
+        string ensName;
+        uint256 reputation;
+        uint256 ethBalance;
+        bool isActive;
+    }
+
+    ENS public ens;
+    mapping(address => PaymasterInfo) public paymasters;
+    address[] public paymasterList;
+
+    event PaymasterRegistered(address indexed paymasterAddress, string ensName);
+    event PaymasterDeposited(address indexed paymasterAddress, uint256 amount);
+    event PaymasterWithdrawn(address indexed paymasterAddress, uint256 amount);
+    event BidPlaced(address indexed paymasterAddress, uint256 bidAmount);
+
+    constructor(address _ensAddress) {
+        ens = ENS(_ensAddress);
+    }
+
+    function registerPaymaster(address _paymasterAddress, string memory _ensName) external {
+        require(paymasters[_paymasterAddress].paymasterAddress == address(0), "Paymaster already registered");
+        
+        // Verify ENS ownership
+        Resolver resolver = Resolver(ens.resolver(keccak256(abi.encodePacked(_ensName))));
+        require(resolver.addr(keccak256(abi.encodePacked(_ensName))) == _paymasterAddress, "ENS name not owned by paymaster");
+
+        paymasters[_paymasterAddress] = PaymasterInfo({
+            paymasterAddress: _paymasterAddress,
+            ensName: _ensName,
+            reputation: 0,
+            ethBalance: 0,
+            isActive: true
+        });
+        paymasterList.push(_paymasterAddress);
+
+        emit PaymasterRegistered(_paymasterAddress, _ensName);
+    }
+
+    function depositETH() external payable {
+        require(paymasters[msg.sender].paymasterAddress != address(0), "Paymaster not registered");
+        paymasters[msg.sender].ethBalance += msg.value;
+        emit PaymasterDeposited(msg.sender, msg.value);
+    }
+
+    function withdrawETH(uint256 _amount) external {
+        require(paymasters[msg.sender].ethBalance >= _amount, "Insufficient balance");
+        paymasters[msg.sender].ethBalance -= _amount;
+        payable(msg.sender).transfer(_amount);
+        emit PaymasterWithdrawn(msg.sender, _amount);
+    }
+
+    function placeBid(uint256 _bidAmount) external {
+        require(paymasters[msg.sender].paymasterAddress != address(0), "Paymaster not registered");
+        // Implement bid logic here
+        emit BidPlaced(msg.sender, _bidAmount);
+    }
+
+    function getLowestBidPaymaster() external view returns (address) {
+        // Implement logic to return the paymaster with the lowest bid
+    }
+
+    function routeUserOperation(/* UserOperation parameters */) external returns (address) {
+        // Implement auto-routing logic here
+        // This function should select the most suitable paymaster based on availability, reputation, and bid amount
+    }
+
+    function updatePaymasterReputation(address _paymasterAddress, uint256 _reputationChange) external onlyOwner {
+        require(paymasters[_paymasterAddress].paymasterAddress != address(0), "Paymaster not registered");
+        paymasters[_paymasterAddress].reputation += _reputationChange;
+    }
+
+    // Additional helper functions and admin functions as needed
+}
